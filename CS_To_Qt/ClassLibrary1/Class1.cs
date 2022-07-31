f2a130968eae3422c57f4dcc3fe45dacb628acb7
小悠 <88084987+kutori-nota-seniorisu@users.noreply.github.com>
2022-07-31 19:32:17 +0800

    Add files via upload

---
+++ CS_To_Qt/ClassLibrary1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public Class1(){}
+        public int myAdd(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+        public void mySub(int a, int b, ref int c)
+        {
+            c = a - b;
+        }
+        public void mySetText(string text)
+        {
+            Console.WriteLine("ClassLibrary1的类名Class1下的mySetText: {0}", text);
+        }
+        public void myGetText(ref string text)
+        {
+            text = "ClassLibrary1的类名Class1下的myGetText";
+        }
+    }
+}
